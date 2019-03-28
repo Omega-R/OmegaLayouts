0a94a93e8d4c53b41d7c81ab182d7cf8647efce3
roman_tcaregorodtcev <roman.tc@omega-r.com>
2019-03-28 10:03:32 +0300

    Disable invalidate shadow view if the same size

--- libs/src/main/java/com/omega_r/libs/layouts/shadow/ShadowLayoutHelper.java
+++ libs/src/main/java/com/omega_r/libs/layouts/shadow/ShadowLayoutHelper.java
@@ -18,7 +18,15 @@ import android.view.ViewGroup;
 
 import com.omega_r.libs.layouts.R;
 
-public class ShadowLayoutHelper {
+class ShadowLayoutHelper {
+
+    private static final int ARRAYS_LENGTH = 4;
+    private static final int LEFT_POSITION = 0;
+    private static final int TOP_POSITION = 1;
+    private static final int RIGHT_POSITION = 2;
+    private static final int BOTTOM_POSITION = 3;
+    private final Drawable[] drawables = new Drawable[ARRAYS_LENGTH];
+    private final int[] sizeArray = new int[ARRAYS_LENGTH];
 
     private final Resources resources;
     private final DisplayMetrics displayMetrics;
@@ -26,17 +34,9 @@ public class ShadowLayoutHelper {
     @ColorInt
     private final int defaultShadowColor;
 
-    private Drawable shadowTopDrawable;
-    private int shadowTopHeight;
-    private Drawable shadowBottomDrawable;
-    private int shadowBottomHeight;
-    private Drawable shadowLeftDrawable;
-    private int shadowLeftHeight;
-    private Drawable shadowRightDrawable;
-    private int shadowRightHeight;
-
-    public ShadowLayoutHelper(@NonNull ViewGroup viewGroup, @NonNull Context context, @Nullable AttributeSet attrs) {
+    ShadowLayoutHelper(@NonNull ViewGroup viewGroup, @NonNull Context context, @Nullable AttributeSet attrs) {
         this.viewGroup = viewGroup;
+
         resources = context.getResources();
         displayMetrics = resources.getDisplayMetrics();
         defaultShadowColor = ContextCompat.getColor(context, R.color.shadow_color_omega);
@@ -51,10 +51,10 @@ public class ShadowLayoutHelper {
     }
 
     private void initHeights(@NonNull TypedArray styledAttrs) {
-        shadowTopHeight = styledAttrs.getDimensionPixelSize(R.styleable.ShadowLayout_shadowTopHeight, 0);
-        shadowBottomHeight = styledAttrs.getDimensionPixelSize(R.styleable.ShadowLayout_shadowBottomHeight, 0);
-        shadowLeftHeight = styledAttrs.getDimensionPixelSize(R.styleable.ShadowLayout_shadowLeftHeight, 0);
-        shadowRightHeight = styledAttrs.getDimensionPixelSize(R.styleable.ShadowLayout_shadowRightHeight, 0);
+        sizeArray[LEFT_POSITION] = styledAttrs.getDimensionPixelSize(R.styleable.ShadowLayout_shadowLeftHeight, 0);
+        sizeArray[TOP_POSITION] = styledAttrs.getDimensionPixelSize(R.styleable.ShadowLayout_shadowTopHeight, 0);
+        sizeArray[RIGHT_POSITION] = styledAttrs.getDimensionPixelSize(R.styleable.ShadowLayout_shadowRightHeight, 0);
+        sizeArray[BOTTOM_POSITION] = styledAttrs.getDimensionPixelSize(R.styleable.ShadowLayout_shadowBottomHeight, 0);
     }
 
     private void initDrawables(@NonNull TypedArray styledAttrs) {
@@ -70,90 +70,95 @@ public class ShadowLayoutHelper {
     }
 
     private void initDrawables(int[] colors) {
-        shadowTopDrawable = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
-        shadowBottomDrawable = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, colors);
-        shadowLeftDrawable = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, colors);
-        shadowRightDrawable = new GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, colors);
+        drawables[LEFT_POSITION] = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, colors);
+        drawables[TOP_POSITION] = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
+        drawables[RIGHT_POSITION] = new GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, colors);
+        drawables[BOTTOM_POSITION] = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, colors);
     }
 
-    public void setShadowColors(@ColorInt int startColor,
-                                @ColorInt int endColor) {
+    void setShadowColors(@ColorInt int startColor, @ColorInt int endColor) {
         initDrawables(new int[] {startColor, endColor});
     }
 
-    public void setShadowColors(@ColorInt int startColor,
-                                @ColorInt int centerColor,
-                                @ColorInt int endColor) {
+    void setShadowColors(@ColorInt int startColor, @ColorInt int centerColor, @ColorInt int endColor) {
         initDrawables(new int[] {startColor, centerColor, endColor});
     }
 
-    public void drawShadows(Canvas canvas) {
+    void drawShadows(Canvas canvas) {
         int width = canvas.getWidth();
         int height = canvas.getHeight();
-        shadowTopDrawable.setBounds(0, 0, width, shadowTopHeight);
-        shadowTopDrawable.draw(canvas);
-        shadowBottomDrawable.setBounds(0, height - shadowBottomHeight, width, height);
-        shadowBottomDrawable.draw(canvas);
-        shadowLeftDrawable.setBounds(0, 0, shadowLeftHeight, height);
-        shadowLeftDrawable.draw(canvas);
-        shadowRightDrawable.setBounds(width - shadowRightHeight, 0, width, height);
-        shadowRightDrawable.draw(canvas);
+
+        Drawable leftDrawable = drawables[LEFT_POSITION];
+        leftDrawable.setBounds(0, 0, sizeArray[LEFT_POSITION], height);
+        leftDrawable.draw(canvas);
+
+        Drawable topDrawable = drawables[TOP_POSITION];
+        topDrawable.setBounds(0, 0, width, sizeArray[TOP_POSITION]);
+        topDrawable.draw(canvas);
+
+        Drawable rightDrawable = drawables[RIGHT_POSITION];
+        rightDrawable.setBounds(width - sizeArray[RIGHT_POSITION], 0, width, height);
+        rightDrawable.draw(canvas);
+
+        Drawable bottomDrawable = drawables[BOTTOM_POSITION];
+        bottomDrawable.setBounds(0, height - sizeArray[BOTTOM_POSITION], width, height);
+        bottomDrawable.draw(canvas);
     }
 
-    public void setShadowsHeightInPixels(int left, int top, int right, int bottom) {
-        shadowLeftHeight = convertPixelsToDp(left);
-        shadowTopHeight = convertPixelsToDp(top);
-        shadowRightHeight = convertPixelsToDp(right);
-        shadowBottomHeight = convertPixelsToDp(bottom);
+    void setShadowsHeightInPixels(int left, int top, int right, int bottom) {
+        sizeArray[LEFT_POSITION] = convertPixelsToDp(left);
+        sizeArray[TOP_POSITION] = convertPixelsToDp(top);
+        sizeArray[RIGHT_POSITION] = convertPixelsToDp(right);
+        sizeArray[BOTTOM_POSITION] = convertPixelsToDp(bottom);
         viewGroup.invalidate();
     }
 
-    public void setShadowsHeightDimens(@DimenRes int left, @DimenRes int top, @DimenRes int right, @DimenRes int bottom) {
-        shadowLeftHeight = resources.getDimensionPixelSize(left);
-        shadowTopHeight = resources.getDimensionPixelSize(top);
-        shadowRightHeight = resources.getDimensionPixelSize(right);
-        shadowBottomHeight = resources.getDimensionPixelSize(bottom);
+    void setShadowsHeightDimens(@DimenRes int left, @DimenRes int top, @DimenRes int right, @DimenRes int bottom) {
+        sizeArray[LEFT_POSITION] = resources.getDimensionPixelSize(left);
+        sizeArray[TOP_POSITION] = resources.getDimensionPixelSize(top);
+        sizeArray[RIGHT_POSITION] = resources.getDimensionPixelSize(right);
+        sizeArray[BOTTOM_POSITION] = resources.getDimensionPixelSize(bottom);
         viewGroup.invalidate();
     }
 
-    public void setLeftShadowInPixels(int size) {
-        shadowLeftHeight = convertPixelsToDp(size);
-        viewGroup.invalidate();
+    void setLeftShadowInPixels(int size) {
+        updateShadowSize(LEFT_POSITION, convertPixelsToDp(size));
     }
 
-    public void setLeftShadowDimens(@DimenRes int resource) {
-        shadowLeftHeight = resources.getDimensionPixelSize(resource);
-        viewGroup.invalidate();
+    void setLeftShadowDimens(@DimenRes int resource) {
+        updateShadowSize(LEFT_POSITION, resources.getDimensionPixelSize(resource));
     }
 
-    public void setRightShadowInPixels(int size) {
-        shadowRightHeight = convertPixelsToDp(size);
-        viewGroup.invalidate();
+    void setRightShadowInPixels(int size) {
+        updateShadowSize(RIGHT_POSITION, convertPixelsToDp(size));
     }
 
-    public void setRightShadowDimens(@DimenRes int resource) {
-        shadowRightHeight = resources.getDimensionPixelSize(resource);
-        viewGroup.invalidate();
+    void setRightShadowDimens(@DimenRes int resource) {
+        updateShadowSize(RIGHT_POSITION, resources.getDimensionPixelSize(resource));
     }
 
-    public void setTopShadowInPixels(int size) {
-        shadowTopHeight = convertPixelsToDp(size);
-        viewGroup.invalidate();
+    void setTopShadowInPixels(int size) {
+        updateShadowSize(TOP_POSITION, convertPixelsToDp(size));
     }
 
-    public void setTopShadowDimens(@DimenRes int resource) {
-        shadowTopHeight = resources.getDimensionPixelSize(resource);
-        viewGroup.invalidate();
+    void setTopShadowDimens(@DimenRes int resource) {
+        updateShadowSize(TOP_POSITION, resources.getDimensionPixelSize(resource));
     }
 
-    public void setBottomShadowInPixels(int size) {
-        shadowBottomHeight = convertPixelsToDp(size);
-        viewGroup.invalidate();
+    void setBottomShadowInPixels(int size) {
+        updateShadowSize(BOTTOM_POSITION, convertPixelsToDp(size));
     }
 
-    public void setBottomShadowDimens(@DimenRes int resource) {
-        shadowBottomHeight = resources.getDimensionPixelOffset(resource);
-        viewGroup.invalidate();
+    void setBottomShadowDimens(@DimenRes int resource) {
+        updateShadowSize(BOTTOM_POSITION, resources.getDimensionPixelSize(resource));
+    }
+
+    private void updateShadowSize(int position, int newSize) {
+        int currentSize = sizeArray[position];
+        if (currentSize != newSize) {
+            sizeArray[position] = newSize;
+            viewGroup.invalidate();
+        }
     }
 
     private int convertPixelsToDp(int px) {
